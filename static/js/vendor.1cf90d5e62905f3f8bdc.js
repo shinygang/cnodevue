e0340d0d905bb7f1ad195ee1c40f2a89d82b4bf7
唐岗 <429517385@qq.com>
2016-11-25 11:18:47 +0800

    update code

--- static/js/vendor.1cf90d5e62905f3f8bdc.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([11,8,10],[]);
-//# sourceMappingURL=vendor.1cf90d5e62905f3f8bdc.js.map
--- static/js/vendors.695837e17db828d559e3.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([10,8,11],[]);
-//# sourceMappingURL=vendors.695837e17db828d559e3.js.map
